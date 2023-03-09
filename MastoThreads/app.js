01cc0692253eea26fd44404c13714fdd448d845c
Michael <30999326+Discostu36@users.noreply.github.com>
2023-03-09 13:47:28 +0100

    Update app.js

--- MastoThreads/app.js
+++ MastoThreads/app.js
@@ -7,22 +7,22 @@ function splitText() {
 
     let maxLength = 500; // Maximale Länge eines Elements im Array
     let result = [];
-    
+
     let currentChunk = "";
     for (let i = 0; i < sentences.length; i++) {
         let sentence = sentences[i];
-    
+
         // Wenn der aktuelle Satz plus das Trennzeichen die maximale Länge überschreitet,
         // füge den aktuellen Chunk zum Ergebnis-Array hinzu und setze den Chunk zurück
         if (currentChunk.length + sentence.length + 2 > maxLength) {
             result.push(currentChunk.trim());
             currentChunk = "";
         }
-    
+
         // Füge den aktuellen Satz zum Chunk hinzu
         currentChunk += sentence + ". ";
     }
-    
+
     // Füge den letzten Chunk zum Ergebnis-Array hinzu
     if (currentChunk) {
         result.push(currentChunk.trim());
@@ -31,9 +31,8 @@ function splitText() {
     result.forEach(textSnippet => {
         let threadPart = document.createElement("textarea");
         threadPart.value = textSnippet;
+        threadPart.rows = 15;
+        threadPart.cols = 40;
         document.body.append(threadPart);
     });
-    
-    console.log(result); // Ausgabe des Ergebnis-Arrays in der Konsole    
 }
-
