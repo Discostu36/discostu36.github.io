579e965c619fd4c0f9bd6344e2d107024ba7d5a5
Michael <30999326+Discostu36@users.noreply.github.com>
2023-03-09 13:09:19 +0100

    Update app.js

--- MastoThreads/app.js
+++ MastoThreads/app.js
@@ -1,26 +1,39 @@
-let sentences = input.split(". "); // Satztrennung durchführen
+const sendButton = document.querySelector("#split-button");
+sendButton.addEventListener("click", splitText);
 
-let maxLength = 500; // Maximale Länge eines Elements im Array
-let result = [];
+function splitText() {
+    let input = document.querySelector("#text-input").innerText;
+    let sentences = input.split(". "); // Satztrennung durchführen
 
-let currentChunk = "";
-for (let i = 0; i < sentences.length; i++) {
-  let sentence = sentences[i];
+    let maxLength = 500; // Maximale Länge eines Elements im Array
+    let result = [];
+    
+    let currentChunk = "";
+    for (let i = 0; i < sentences.length; i++) {
+        let sentence = sentences[i];
+    
+        // Wenn der aktuelle Satz plus das Trennzeichen die maximale Länge überschreitet,
+        // füge den aktuellen Chunk zum Ergebnis-Array hinzu und setze den Chunk zurück
+        if (currentChunk.length + sentence.length + 2 > maxLength) {
+            result.push(currentChunk.trim());
+            currentChunk = "";
+        }
+    
+        // Füge den aktuellen Satz zum Chunk hinzu
+        currentChunk += sentence + ". ";
+    }
+    
+    // Füge den letzten Chunk zum Ergebnis-Array hinzu
+    if (currentChunk) {
+        result.push(currentChunk.trim());
+    }
 
-  // Wenn der aktuelle Satz plus das Trennzeichen die maximale Länge überschreitet,
-  // füge den aktuellen Chunk zum Ergebnis-Array hinzu und setze den Chunk zurück
-  if (currentChunk.length + sentence.length + 2 > maxLength) {
-    result.push(currentChunk.trim());
-    currentChunk = "";
-  }
-
-  // Füge den aktuellen Satz zum Chunk hinzu
-  currentChunk += sentence + ". ";
+    result.forEach(textSnippet => {
+        let textArea = document.createElement(textArea);
+        textArea.innerText = textSnippet;
+        document.querySelector("body").append(textArea);
+    });
+    
+    console.log(result); // Ausgabe des Ergebnis-Arrays in der Konsole    
 }
 
-// Füge den letzten Chunk zum Ergebnis-Array hinzu
-if (currentChunk) {
-  result.push(currentChunk.trim());
-}
-
-console.log(result); // Ausgabe des Ergebnis-Arrays in der Konsole
